46a7ba7c91554def73ec49eb6c37a715807b7160
rafael lisboa <rafaelzlisboa@gmail.com>
2015-05-07 18:03:55 -0300

    bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='leap.auth',
-      version='0.1.0',
+      version='0.1.2',
       description='Library to authenticate with a LEAP platform',
       long_description=read('README.rst'),
       license="GNU Affero General Public License v3",
